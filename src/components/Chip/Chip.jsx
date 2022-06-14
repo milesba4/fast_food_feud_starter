50e3cc0db7ad5c0979103019300216199a40d111
milesba4 <milesbaffour@gmail.com>
2022-06-14 18:44:33 -0400

    Adding logic for close button

--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -1,17 +1,16 @@
 import * as React from "react"
 import "./Chip.css"
 
-export function Chip({ buttonClassName="", label = "", isActive = true, onClick=()=>{}}) {
+export function Chip({ buttonClassName="", label = "", isActive = true, onClick=()=>{}, onClose=()=>{}}) {
   if(isActive == false){ 
   buttonClassName = "chip";
   }else{ buttonClassName = "chip active";
   }
 
   return (
-    <button className={buttonClassName} onClick = {onclick}>
     <button className={buttonClassName} onClick = {onClick}>
       <p className="label">{label}</p>
-      <span className="close" role="button">{`X`}</span>
+      <span className="close" role="button" onClick={onClose}>{`X`}</span>
     </button>
   )
 }
